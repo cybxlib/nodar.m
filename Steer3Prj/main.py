434763f27a2d8b1742368bc6cfd7177d5cd4c618
cybxlib <97873936+cybxlib@users.noreply.github.com>
2025-08-19 10:39:43 +0400

    Update main.py

--- Steer3Prj/main.py
+++ Steer3Prj/main.py
@@ -6,21 +6,26 @@ import curve_examples
 import curve_plotting
 
 if __name__ == "__main__":
-    l_values = torch.linspace(0, 10 * torch.pi, 500)  # Arc length values
+    # l_values = torch.linspace(0, 10 * torch.pi, 500)  # Arc length values
+    l_values = torch.linspace(0, 10 * torch.pi, 2500, device='cpu', dtype=torch.float32)
 
     # Example Usage
-    v, tv = curve_examples.helix(l_values)
-    x, y, z = f_s(l_values, v, tv)
-    curve_plotting.plot_curve(x, y, z, '3D Curve (Helix Example)')
+    curvature, torsion = curve_examples.straight_line(l_values)
+    x_straight, y_straight, z_straight = frenet_serret(l_values, curvature, torsion)
+    curve_plotting.plot_curve(x_straight, y_straight, z_straight, "Straight Line in 3D")
 
-    v, tv = curve_examples.screw(l_values)
-    x, y, z = f_s(l_values, v, tv)
-    curve_plotting.plot_curve(x, y, z, '3D Curve (Screw Example)')
-
-    v, tv = curve_examples.circle(l_values)
-    x_circle, y_circle, z_circle = f_s(l_values, v, tv)
+    curvature, torsion = curve_examples.circle(l_values)
+    x_circle, y_circle, z_circle = frenet_serret(l_values, curvature, torsion)
     curve_plotting.plot_curve(x_circle, y_circle, z_circle, "3D Circle")
 
-    v, tv = curve_examples.straight_line(l_values)
-    x_straight, y_straight, z_straight = f_s(l_values, v, tv)
-    curve_plotting.plot_curve(x_straight, y_straight, z_straight, "Straight Line in 3D")
+    curvature, torsion = curve_examples.helix(l_values)
+    x, y, z = frenet_serret(l_values, curvature, torsion)
+    curve_plotting.plot_curve(x, y, z, '3D Curve (Helix Example)')
+
+    curvature, torsion = curve_examples.screw(l_values)
+    x, y, z = frenet_serret(l_values, curvature, torsion)
+    curve_plotting.plot_curve(x, y, z, '3D Curve (Screw Example)')
+
+    curvature, torsion = curve_examples.tourch_cube(l_values)
+    x_straight, y_straight, z_straight = frenet_serret(l_values, curvature, torsion)
+    curve_plotting.plot_curve(x_straight, y_straight, z_straight, '3D Curve (Cubic Parabola)')
